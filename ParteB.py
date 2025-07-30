9d4a4dd075a10d8b722d490d01084c41a93cc8a1
Raquel Mora <raquel.mora@ulead.ac.cr>
2025-07-30 11:31:17 -0600

    parte B mejor comentada

--- ParteB.py
+++ ParteB.py
@@ -5,30 +5,28 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()  # número del proceso
 size = comm.Get_size()  # total de procesos
 
-# Asegurarse de que solo haya 2 procesos
+# Asegurarse de que solo haya 2 procesos a la hora de correr en la terminal
 if size != 2:
     if rank == 0:
         print("Este programa necesita exactamente 2 procesos.")
     exit()
 
-# Número de veces que se va a enviar el mensaje
-N = 10000
 
-# Mensaje de 1 byte
-mensaje = np.zeros(1, dtype='b')  # 'b' = entero de 1 byte
+N = 10000 # número de veces que se va a enviar el mensaje
+mensaje = np.zeros(1, dtype='b')  # b = entero de 1 byte
 
 # Proceso 0 envía y recibe el mensaje N veces
 if rank == 0:
     start_time = MPI.Wtime()  # tiempo inicial
 
-    for i in range(N):
-        comm.Send(mensaje, dest=1, tag=0)     # enviar a proceso 1
-        comm.Recv(mensaje, source=1, tag=0)   # recibir respuesta
+    for i in range(N): # recordar que N es cantidad de veces que se manda y se recibe el mensaje 
+        comm.Send(mensaje, dest=1, tag=0)     # MPI_Send: proceso 0 envia 1 byte a proceso 0
+        comm.Recv(mensaje, source=1, tag=0)   # MPI_Recv: proceso 0 recibe respuesta de proceso 1
 
     end_time = MPI.Wtime()  # tiempo final
     total_time = end_time - start_time
 
-    # Calcular latencias
+    # Calcular latencias (después de completar las N transmisiones de ida y vuelta )
     latencia_promedio = (total_time / N) * 1e6  # en microsegundos
     latencia_unidireccional = latencia_promedio / 2
 
@@ -39,5 +37,5 @@ if rank == 0:
 # Proceso 1 recibe el mensaje y lo devuelve
 elif rank == 1:
     for i in range(N):
-        comm.Recv(mensaje, source=0, tag=0)   # recibir mensaje
-        comm.Send(mensaje, dest=0, tag=0)     # enviar de vuelta
+        comm.Recv(mensaje, source=0, tag=0)   # MPI_RCV:ecibir mensaje de proceso 0 
+        comm.Send(mensaje, dest=0, tag=0)     # MPI_Send:enviar de vuelta a proceso 0
